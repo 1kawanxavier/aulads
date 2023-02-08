c85fb245797eccd5c2156f1840038c4cf9e5763d
1kawanxavier <123658645+1kawanxavier@users.noreply.github.com>
2023-02-08 23:10:07 +0000

    Primeiro codigo git

---
+++ exe01.py
@@ -0,0 +1,14 @@
+n1 = float(input("Informa a 1º nota:"))
+n2 = float(input("Informa a 2º nota:"))
+n3 = float(input("Informa a 3º nota:"))
+me = float(input("Informa a ME:"))
+
+ma = (n1 + n2 * 2 + n3 * 3 + me)/7
+
+if (ma>=9):
+    print ( "Maior ou igual a 9")
+
+if (ma<4):
+    print ("Menor que 4")
+
+
